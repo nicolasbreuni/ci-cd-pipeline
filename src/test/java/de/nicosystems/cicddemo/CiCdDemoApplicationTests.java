932bcc2695005279116fd6b0dcfd216f0955c440
Nicolas Breuninger <nicolas@fambreuni.de>
2020-12-01 22:18:54 +0100

    Update test

--- src/test/java/de/nicosystems/cicddemo/CiCdDemoApplicationTests.java
+++ src/test/java/de/nicosystems/cicddemo/CiCdDemoApplicationTests.java
@@ -3,11 +3,14 @@ package de.nicosystems.cicddemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class CiCdDemoApplicationTests {
 
     @Test
     void contextLoads() {
+        assertThat(1).isEqualTo(1);
     }
 
 }
