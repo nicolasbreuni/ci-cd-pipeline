91ac04f0f943c3e1623ce3ab208152ee00c92673
Nicolas Breuninger <nicolas@fambreuni.de>
2020-12-10 16:07:56 +0100

    Test

--- src/main/java/de/nicosystems/cicddemo/CiCdDemoApplication.java
+++ src/main/java/de/nicosystems/cicddemo/CiCdDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CiCdDemoApplication {
 
     public static void main(String[] args) {
+
+        //test
         SpringApplication.run(CiCdDemoApplication.class, args);
     }
 
